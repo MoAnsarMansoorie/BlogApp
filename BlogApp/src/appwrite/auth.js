2bf6928222f55b153273ae4ec3296f4a0b1609d1
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2023-12-04 22:48:48 +0530

    authAppwrite

--- BlogApp/src/appwrite/auth.js
+++ BlogApp/src/appwrite/auth.js
@@ -1,4 +1,59 @@
 import config from "../config/config";
 import {Client, Account, ID} from "appwrite"
 
-export const AuthService {}
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique, email, password, name)
+            if(userAccount){
+                // call another method
+            }
+            else {
+                return userAccount
+            }
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService()
+
+export default authService;
