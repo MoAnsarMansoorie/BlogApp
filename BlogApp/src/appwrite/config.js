5dccd9d4edd8b0d804efe5ef91684ca95c4b393c
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2023-12-05 19:54:12 +0530

    configAppwrite

--- BlogApp/src/appwrite/config.js
+++ BlogApp/src/appwrite/config.js
@@ -1,10 +1,39 @@
 import config from "../config/config";
-import { Client, ID,Databases, Storage, Query,  } from "appwrite";
+import { Client, ID, Databases, Storage, Query,  } from "appwrite";
 
 export class Service{
     client = new Client();
+    databases;
+    bucket;
+    
+    constructor(){
+        this.client
+        .setEndpoint(config.appwriteUrl)
+        .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: createPost :: error", error);
+        }
+    }
     
 }
 
 const serive = new Service()
-export default Service
+export default serive  
