254bd4f6fe643770c1c908c5d3046f87916f6517
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2023-12-06 21:16:18 +0530

    appFile

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -4,6 +4,7 @@ import {useDispatch} from "react-redux"
 import './App.css'
 import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -23,7 +24,17 @@ function App() {
   },[])
 
 
-  return !loading ? <div></div> : null
+  return !loading ? 
+        <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+          <div className='w-full block'>
+            <Header />
+            <main>
+              TODO: {/* <Outlet /> */}
+            </main>
+            <Footer />
+          </div>
+        </div> 
+  : null
 }
 
 export default App
